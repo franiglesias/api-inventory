d5797322200d3073d7a9619a5ae5427c97000e72
Fran Iglesias <franiglesias@mac.com>
2025-08-31 20:10:58 +0200

    First commit with a basic configuration of the server

---
+++ src/controllers/HealthController.ts
@@ -0,0 +1,10 @@
+import { Request, Response } from 'express'
+
+/**
+ * A simple controller demonstrating a healthcheck endpoint.
+ */
+export default class HealthController {
+  public handle(req: Request, res: Response): void {
+    res.status(200).json({ status: 'ok' })
+  }
+}

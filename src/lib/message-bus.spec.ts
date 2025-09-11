903979a61f6ecfee5f0ecb9cfcc82d091711dfd7
Fran Iglesias <franiglesias@mac.com>
2025-09-11 19:03:18 +0200

    Adding a simple message bus as a library

---
+++ src/lib/message-bus.spec.ts
@@ -0,0 +1,40 @@
+import {describe, expect, it} from "vitest";
+import {Message} from "./message";
+import {MessageHandler, MessageBus} from "./message.bus";
+import {UnsupportedCommand} from "./unsupported.command";
+
+class UnknownCommand implements Message {
+}
+class ACommand implements Message {}
+
+class ACommandHandler implements MessageHandler<ACommand>{
+    handle(command: ACommand) {
+        throw new Error("Method not implemented.");
+    }
+
+}
+
+describe('MessageBus', () => {
+    describe('When invoked', () => {
+        it('should fail if not registered command', () => {
+            const bus = new MessageBus()
+            console.log('Unknown command class name:', UnknownCommand.name) // prints "UnknownCommand"
+            expect(() => bus.dispatch(new UnknownCommand())).toThrow(UnsupportedCommand)
+        });
+
+        it('should fail if registering unsupported', () => {
+            const bus = new MessageBus()
+
+            bus.register(UnknownCommand, new ACommandHandler())
+            expect(() => bus.dispatch(new UnknownCommand())).toThrowError("Method not implemented.")
+        })
+
+        it('should pass if registered command', () => {
+            const bus = new MessageBus()
+
+            bus.register(ACommand, new ACommandHandler())
+            expect(() => bus.dispatch(new ACommand())).toThrowError("Method not implemented.")
+        })
+
+    })
+});

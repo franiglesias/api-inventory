ab18e73f6a689c5009b87aca9c4d1b20e2224854
Fran Iglesias <franiglesias@mac.com>
2025-09-13 13:28:13 +0200

    Fix formatting problems

--- src/driven/forStoringProducts/MemoryAdapter.spec.ts
+++ src/driven/forStoringProducts/MemoryAdapter.spec.ts
@@ -1,47 +1,47 @@
-import {describe, it,  expect} from "vitest";
-import {StoredProduct} from "../../inventory/driven/forStoringProducts/ForStoringProducts";
-import {ForStoringProductsMemoryAdapter} from "./MemoryAdapter";
+import { describe, it, expect } from "vitest"
+import { StoredProduct } from "../../inventory/driven/forStoringProducts/ForStoringProducts"
+import { ForStoringProductsMemoryAdapter } from "./MemoryAdapter"
 
-describe('ForStoringProductsMemoryAdapter', () => {
-    describe('When storing a product with new id', () => {
-        it('should add a new entry', () => {
-            const product = new StoredProduct()
-            product.id = "e5057e6d-cdc0-490e-857e-6dcdc0b90e08"
-            product.sku = "SKU"
-            product.name = "Name"
-            product.description = "Description"
-            product.stock = 10
-            product.minStock = 5
-            product.createdAt = new Date()
-            product.updatedAt = new Date()
-            const forStoringProducts = new ForStoringProductsMemoryAdapter([])
-            forStoringProducts.store(product)
+describe("ForStoringProductsMemoryAdapter", () => {
+  describe("When storing a product with new id", () => {
+    it("should add a new entry", () => {
+      const product = new StoredProduct()
+      product.id = "e5057e6d-cdc0-490e-857e-6dcdc0b90e08"
+      product.sku = "SKU"
+      product.name = "Name"
+      product.description = "Description"
+      product.stock = 10
+      product.minStock = 5
+      product.createdAt = new Date()
+      product.updatedAt = new Date()
+      const forStoringProducts = new ForStoringProductsMemoryAdapter([])
+      forStoringProducts.store(product)
 
-            expect(forStoringProducts.retrieveAll()).toHaveLength(1)
-        });
+      expect(forStoringProducts.retrieveAll()).toHaveLength(1)
     })
+  })
 
-    describe('When storing a product with existing id', () => {
-        it('should update the entry', () => {
-            const product = new StoredProduct()
-            product.id = "e5057e6d-cdc0-490e-857e-6dcdc0b90e08"
-            product.sku = "SKU"
-            product.name = "Product Name"
-            product.description = "First version of the product"
-            product.stock = 10
-            product.minStock = 5
-            product.createdAt = new Date()
-            product.updatedAt = new Date()
+  describe("When storing a product with existing id", () => {
+    it("should update the entry", () => {
+      const product = new StoredProduct()
+      product.id = "e5057e6d-cdc0-490e-857e-6dcdc0b90e08"
+      product.sku = "SKU"
+      product.name = "Product Name"
+      product.description = "First version of the product"
+      product.stock = 10
+      product.minStock = 5
+      product.createdAt = new Date()
+      product.updatedAt = new Date()
 
-            const forStoringProducts = new ForStoringProductsMemoryAdapter([])
-            forStoringProducts.store(product)
+      const forStoringProducts = new ForStoringProductsMemoryAdapter([])
+      forStoringProducts.store(product)
 
-            product.name = "Product Name Updated"
-            product.description = "Second version of the product"
+      product.name = "Product Name Updated"
+      product.description = "Second version of the product"
 
-            forStoringProducts.store(product)
+      forStoringProducts.store(product)
 
-            expect(forStoringProducts.retrieveAll()).toHaveLength(1)
-        })
+      expect(forStoringProducts.retrieveAll()).toHaveLength(1)
     })
+  })
 })
